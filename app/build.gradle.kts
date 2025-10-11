a70dff217c2a8e5bbea32bee04751191eee0ca0d
Peoples2448 <danieldawda2448@gmail.com>
2025-10-10 19:50:08 -0700

    messing with gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.github.lecho:hellocharts-library:1.5.8@aar")
+    implementation("com.github.lecho:hellocharts-android:v1.5.8")
 }
--- app/src/main/java/com/example/daniel_dawda_stressmeter/ui/stress_meter/StressMeterFragment.kt
+++ app/src/main/java/com/example/daniel_dawda_stressmeter/ui/stress_meter/StressMeterFragment.kt
@@ -1,5 +1,6 @@
 package com.example.daniel_dawda_stressmeter.ui.stress_meter
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,8 @@ import com.example.daniel_dawda_stressmeter.databinding.FragmentSlideshowBinding
 import com.example.daniel_dawda_stressmeter.ui.home.GridViewAdapter
 import com.example.daniel_dawda_stressmeter.ui.home.HomeFragment.StressData
 import lecho.lib.hellocharts.model.Line
+import lecho.lib.hellocharts.model.LineChartData
+import lecho.lib.hellocharts.model.PointValue
 import lecho.lib.hellocharts.view.LineChartView
 import java.io.File
 import java.io.FileInputStream
@@ -42,15 +45,22 @@ class StressMeterFragment : Fragment() {
         val listView: ListView = binding.stressList
 
         val file = File(requireContext().filesDir, "stress_data.csv")
-        val stressList: List<StressData> = readCsv(FileInputStream(file))
+        val stressList: List<StressData> = readCsv(FileInputStream(file), false) as List<StressData>
 
         val adapter = StressAdapter(requireContext(), stressList as ArrayList<StressData>)
         listView.adapter = adapter
 
-        // line chart
+        // line chart adapted from https://github.com/lecho/hellocharts-android Usage
         val chart : LineChartView = binding.lineChart
-        val line : Line = Line()
+        val chartValues: List<PointValue> = readCsv(FileInputStream(file), true) as List<PointValue>
 
+        val line = Line(chartValues).setColor(Color.BLUE).setCubic(true)
+        val lines = listOf(line)
+
+        val data = LineChartData()
+        data.lines = lines
+
+        chart.lineChartData = data
 
         return root
     }
@@ -61,13 +71,25 @@ class StressMeterFragment : Fragment() {
     }
 
     // csv read and write adapted from https://www.baeldung.com/kotlin/csv-files
-    fun readCsv(inputStream: InputStream): List<StressData> {
+    fun readCsv(inputStream: InputStream, chart : Boolean): List<Any> {
         val reader = inputStream.bufferedReader()
-        return reader.lineSequence()
-            .filter { it.isNotBlank() }
-            .map {
-                val (timeStamp, stressLevel) = it.split(',', ignoreCase = false, limit = 2)
-                StressData(timeStamp.trim().toLong(), stressLevel.trim().toInt())
-            }.toList()
+        // read into chart data
+        if (chart) {
+            return reader.lineSequence()
+                .filter { it.isNotBlank() }
+                .map {
+                    val (timeStamp, stressLevel) = it.split(',', ignoreCase = false, limit = 2)
+                    PointValue(timeStamp.trim().toFloat(), stressLevel.trim().toFloat())
+                }.toList()
+        }
+        // read into StressData
+        else {
+            return reader.lineSequence()
+                .filter { it.isNotBlank() }
+                .map {
+                    val (timeStamp, stressLevel) = it.split(',', ignoreCase = false, limit = 2)
+                    StressData(timeStamp.trim().toLong(), stressLevel.trim().toInt())
+                }.toList()
+        }
     }
 }
